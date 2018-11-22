e5daa8b185cbdc37377c0016c6e5680a8fcb2bd5
Adam Petrie <adam.petrie@prodigygame.com>
2018-11-22 13:30:34 -0500

    Use Listr for managing tasks

--- index.js
+++ index.js
@@ -1,67 +1,7 @@
 const homeDir = require("os").homedir();
 const fs = require("fs");
-const NodeGit = require("nodegit");
-const { spawn } = require("child_process");
-
-const ora = require("ora");
-
-const root = `${homeDir}/prodigy`;
-if (!fs.existsSync(root)) {
-  fs.mkdirSync(root);
-  console.log("Creating prodigy directory...");
-}
-
-const reposDir = `${homeDir}/prodigy/repos`;
-if (!fs.existsSync(reposDir)) {
-  fs.mkdirSync(reposDir);
-  console.log("Creating prodigy/repos directory...");
-}
-
-const cloneOptions = {};
-cloneOptions.fetchOpts = {
-  callbacks: {
-    certificateCheck: function() {
-      return 1;
-    },
-    credentials: function(url, userName) {
-      return NodeGit.Cred.sshKeyFromAgent(userName);
-    }
-  }
-};
-
-function cloneRepo(repo) {
-  const { remote, name } = repo;
-  const localPath = `${homeDir}/prodigy/repos/${name}`;
-  const spinner = ora(`Cloning ${name}...`).start();
-
-  NodeGit.Clone(remote, localPath, cloneOptions)
-    .then(result => {
-      spinner.succeed();
-      installDeps(repo);
-    })
-    .catch(err => {
-      console.log(err);
-      spinner.stop();
-    });
-}
-
-function installDeps(repo) {
-  const { name } = repo;
-  const spinner2 = ora(`Installing ${name} dependencies...`).start();
-  const child = spawn("yarn install", {
-    shell: true,
-    cwd: `${homeDir}/prodigy/repos/${name}`
-  });
-
-  child.on("error", err => {
-    console.log(err);
-    console.log("Done");
-  });
-
-  child.on("exit", err => {
-    spinner2.succeed();
-  });
-}
+const Listr = require("listr");
+const execa = require("execa");
 
 const repos = [
   {
@@ -74,6 +14,60 @@ const repos = [
   }
 ];
 
-repos.forEach(repo => {
-  cloneRepo(repo);
-});
+function repoTasks(repo) {
+  return new Listr([
+    {
+      title: "Cloning Repo...",
+      task: async ctx => {
+        await execa("node", ["./clone.js", repo.remote, repo.name]);
+      }
+    },
+    {
+      title: "Installing dependencies...",
+      task: async () => {
+        await execa("yarn", ["install"], {
+          cwd: `${homeDir}/prodigy/${repo.name}`
+        });
+      }
+    }
+  ]);
+}
+
+function installRepos() {
+  const tasks = repos.map(repo => {
+    return {
+      title: repo.name,
+      task: () => {
+        return repoTasks(repo);
+      }
+    };
+  });
+
+  return new Listr(tasks, { concurrent: true });
+}
+
+const setup = new Listr(
+  [
+    {
+      title: "Creating prodigy directory",
+      task: (ctx, task) => {
+        const root = `${homeDir}/prodigy`;
+        if (!fs.existsSync(root)) {
+          fs.mkdirSync(root);
+          task.title = `Created ${root}`;
+        } else {
+          task.title = `${root} already exists`;
+        }
+      }
+    },
+    {
+      title: "Setting up local repositories",
+      task: () => {
+        return installRepos();
+      }
+    }
+  ],
+  { collapse: false }
+);
+
+setup.run();
